76b86465d6e3f2e97a2db976319617c6c177eacc
qqhpc <43428900+qqhpc@users.noreply.github.com>
2022-11-16 17:19:16 +0800

    Update build-glinet-official-firmware.sh

--- scripts/build-glinet-official-firmware.sh
+++ scripts/build-glinet-official-firmware.sh
@@ -25,6 +25,8 @@ echo "Start..."
 
 #clone source tree 
 git clone https://github.com/gl-inet/gl-infra-builder.git $base/gl-infra-builder
+ln -sf /home/runner/gl-infra-builder /workdir/gl-infra-builder
+ls /workdir/gl-infra-builder
 cp -r custom/  $base/gl-infra-builder/feeds/custom/
 cp -r *.yml $base/gl-infra-builder/profiles
 cd $base/gl-infra-builder

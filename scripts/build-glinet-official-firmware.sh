2e31ae41165c7bcd1bac5011e94360e798e2f2e7
qqhpc <43428900+qqhpc@users.noreply.github.com>
2022-11-16 10:20:35 +0800

    Update build-glinet-official-firmware.sh

--- scripts/build-glinet-official-firmware.sh
+++ scripts/build-glinet-official-firmware.sh
@@ -8,6 +8,16 @@ ui=$3
 
 echo $base
 
+if [ ! -e "$base" ]; then
+	echo "Please enter base folder"
+	exit 1
+else
+	if [ ! -d $base ]; then 
+		echo "Openwrt base folder not exist"
+		exit 1
+	fi
+fi
+
 if [ ! -n "$profile" ]; then
 	profile=target_wlan_ap-gl-ax1800
 fi
@@ -22,55 +32,92 @@ pwd
 echo "当前目录内容"
 ls -alh
 
-echo "进入目标目录"
-cd /home/runner/work/build-gl.inet/build-gl.inet/
+echo "克隆 glinet 源码"
+git clone https://github.com/qqhpc/gl-inet-gl-infra-builder.git $base/gl-infra-builder
 
-echo "复制自定义插件源码至官方的插件目录"
-cp -r custom/  /workdir/gl-infra-builder/feeds/custom/
+echo "将软件源码和依赖复制进目的目录"
+cp -r custom/  $base/gl-infra-builder/feeds/custom/
 
-echo "复制插件自定义配置文件至官方的配置目录"
-cp -r *.yml /workdir/gl-infra-builder/profiles/
+echo "将自定义配置文件复制进目标目录"
+cp -r *.yml $base/gl-infra-builder/profiles
 
-cd /workdir/gl-infra-builder
+cd $base/gl-infra-builder
+#cd /workdir/gl-infra-builder
 
-echo "选择内核版本并下载对应源码"
-if [[ $profile == *5-4* ]]; then
-        python3 setup.py -c configs/config-wlan-ap-5.4.yml
-else
-        python3 setup.py -c configs/config-wlan-ap.yml
-fi
+function build_firmware(){
+    cd ~/openwrt
+    need_gl_ui=$1
+    ui_path=$2
+	# 替换为高版本go
+    rm -rf feeds/packages/lang/golang
+    svn co https://github.com/openwrt/packages/branches/openwrt-22.03/lang/golang feeds/packages/lang/golang
+    #install feed 
+    ./scripts/feeds update -a && ./scripts/feeds install -a && ./scripts/feeds install -a -f && make defconfig
+    #build 
+    if [[ $need_gl_ui == true  ]]; then 
+        make -j$(expr $(nproc) + 1) GL_PKGDIR=~/glinet/$ui_path/ V=s
+    else
+        make -j$(expr $(nproc) + 1)  V=s
+    fi
+}
 
-cp -r /home/runner/work/build-gl.inet/build-gl.inet/custom.yml /workdir/gl-infra-builder/wlan-ap/openwrt/
+function copy_file(){
+	patch=$1
+	mkdir ~/firmware
+	mkdir ~/packages
+	cd $patch
+	rm -rf packages
+	cp -rf ./* ~/firmware
+}
 
-echo "进入目标目录"
-cd wlan-ap/openwrt
-
-echo "更新插件源码"
-./scripts/gen_config.py $profile glinet_depends custom
-
-echo "克隆glinet私有软件包"
-git clone https://github.com/qqhpc/gl-inet-glinet4.x.git /workdir/gl-infra-builder/glinet
-
-echo "下载feeds"
-./scripts/feeds update -a
-
-echo "更新Golang"
-rm -rf feeds/packages/lang/golang
-svn co https://github.com/openwrt/packages/branches/openwrt-22.03/lang/golang feeds/packages/lang/golang
-
-echo "安装feeds packages"
-./scripts/feeds install -a
-
-echo "安装feeds packages again"
-./scripts/feeds install -a
-
-echo "生成配置文件"
-make defconfig
-
-echo ""
-if [[ $ui == true  ]]; then 
-	make -j$(expr $(nproc) + 1) GL_PKGDIR=/workdir/gl-infra-builder/glinet/ipq60xx/ V=s
-else
-	make -j$(expr $(nproc) + 1)  V=s
-fi
-
+case $profile in 
+    target_wlan_ap-gl-ax1800|\
+    target_wlan_ap-gl-axt1800|\
+    target_wlan_ap-gl-ax1800-5-4|\
+    target_wlan_ap-gl-axt1800-5-4)
+        if [[ $profile == *5-4* ]]; then
+            python3 setup.py -c configs/config-wlan-ap-5.4.yml
+        else
+            python3 setup.py -c configs/config-wlan-ap.yml
+        fi
+	
+        ln -s $base/gl-infra-builder/wlan-ap/openwrt ~/openwrt && cd ~/openwrt
+	
+        if [[ $ui == true  ]]; then 
+	        ./scripts/gen_config.py $profile glinet_depends custom
+            git clone https://github.com/gl-inet/glinet4.x.git ~/glinet
+        else
+	        ./scripts/gen_config.py $profile openwrt_common luci custom
+        fi
+	
+        build_firmware $ui ipq60xx
+		copy_file ~/openwrt/bin/targets/*/*
+    ;;
+    target_ipq40xx_gl-a1300)
+        python3 setup.py -c configs/config-21.02.2.yml
+        ln -s $base/gl-infra-builder/openwrt-21.02/openwrt-21.02.2 ~/openwrt && cd ~/openwrt
+        ./scripts/gen_config.py $profile openwrt_common luci custom
+        build_firmware 
+		copy_file ~/openwrt/bin/targets/*/*
+    ;;
+	target_mt7981_gl-mt2500)
+		python3 setup.py -c configs/config-mt798x-7.6.6.1.yml
+		ln -s $base/gl-infra-builder/mt7981 ~/openwrt && cd ~/openwrt	
+		if [[ $ui == true  ]]; then
+            ./scripts/gen_config.py $profile glinet_depends custom
+            git clone https://github.com/gl-inet/glinet4.x.git ~/glinet
+        else
+            ./scripts/gen_config.py $profile custom
+        fi
+		build_firmware $ui mt7981
+		copy_file ~/openwrt/bin/targets/*/*
+	;;
+	target_siflower_gl-sf1200|\
+	target_siflower_gl-sft1200)
+		python3 setup.py -c configs/config-siflower-18.x.yml
+		ln -s $base/gl-infra-builder/openwrt-18.06/siflower/openwrt-18.06 ~/openwrt && cd ~/openwrt
+		./scripts/gen_config.py $profile custom
+		build_firmware
+		copy_file ~/openwrt/bin/targets/*
+	;;
+esac
